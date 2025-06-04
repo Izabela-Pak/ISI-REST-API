65a591e52aac3757a8be5210aaf94fbdff2a84c0
Izabela-Pak <iza2002r@gmail.com>
2025-06-04 15:03:48 +0200

    zmiany

--- bingo-eurowizyjne/Server/server.js
+++ bingo-eurowizyjne/Server/server.js
@@ -13,6 +13,13 @@ db.sequelize.sync()
         console.log(`Błąd ${error}`);
     });
 
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+    
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
